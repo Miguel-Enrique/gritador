a9b68a9ccb26b25aba3505fa96b9664cd6271448
miguel-enrique <miguelxzsmaster@gmail.com>
2020-03-30 20:27:57 -0300

    gritador completo

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite algo para mim gritar");
+            string texto;
+            texto = Console.ReadLine();
+            Console.WriteLine(texto.ToUpper());
+            Console.WriteLine(texto);
+            
+        }
+    }
+}
